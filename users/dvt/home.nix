f30df2b5d3cac1221a337879d4b0b71ba20e650e
Diego Velez <diego.velez.dev@gmail.com>
2026-05-03 22:42:16 -0400

    feat: add image viewer

--- users/dvt/home.nix
+++ users/dvt/home.nix
@@ -70,6 +70,39 @@ in
     size = 26;
   };
 
+  programs.imv = {
+    enable = true;
+    settings = {
+      options = {
+        background = "282A36";
+      };
+
+      binds = {
+        q = "quit";
+        c = "center";
+        r = "reset";
+        y = ''exec wl-copy "$imv_current_file"'';
+
+        # Panning
+        "<Left>" = "pan 50 0";
+        "<Right>" = "pan -50 0";
+        "<Up>" = "pan 0 50";
+        "<Down>" = "pan 0 -50";
+
+        # Image navigation
+        n = "next";
+        p = "prev";
+        gg = "goto 1";
+        "<Shift+G>" = "goto -1";
+
+        # Zooming
+        i = "zoom 1";
+        o = "zoom -1";
+        a = "zoom actual";
+      };
+    };
+  };
+
   programs.zen-browser = {
     enable = true;
     languagePacks = [ "en-US" ];
