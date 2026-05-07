eb37b1d4a61ea6d4aa9a241d0e1aa03187c1d36e
Diego Velez <diego.velez.dev@gmail.com>
2026-05-07 14:59:33 -0400

    feat: enable steam

--- hosts/common.nix
+++ hosts/common.nix
@@ -97,6 +97,13 @@
   };
 
   programs.localsend.enable = true;
+  programs.steam = {
+    enable = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
+  };
+  programs.gamemode.enable = true;
 
   nixpkgs.config.allowUnfree = true;
   nix.settings = {
