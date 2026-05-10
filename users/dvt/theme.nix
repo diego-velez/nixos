78e032d81b811c028430164d309332a7a03c0920
Diego Velez <diego.velez.dev@gmail.com>
2026-05-09 20:37:01 -0400

    fix: silence warning

--- users/dvt/theme.nix
+++ users/dvt/theme.nix
@@ -16,6 +16,16 @@
       name = "Dracula";
       package = pkgs.dracula-icon-theme;
     };
+
+    # This silences the following warning:
+    #
+    #     evaluation warning:
+    #     The default value of `gtk.gtk4.theme` has changed from `config.gtk.theme` to `null`.
+    #     You are currently using the legacy default (`config.gtk.theme`)
+    #     because `home.stateVersion` is less than "26.05".
+    #     To silence this warning and keep legacy behavior, set: gtk.gtk4.theme = config.gtk.theme;
+    #     To adopt the new default behavior, set: gtk.gtk4.theme = null;
+    gtk4.theme = null;
   };
 
   home.pointerCursor = {
